fb7646ff28e3086b2d80ec317439344d7b329279
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-10 18:18:04 +0200

    fixing reqs

--- config/python.py
+++ config/python.py
@@ -4,16 +4,17 @@ from typing import List
 console_scripts: List[str] = [
     "pyocutil=pyocutil.main:main",
 ]
-config_requires: List[str] = []
 dev_requires: List[str] = [
     "pypitools",
+]
+config_requires: List[str] = [
+    "pyclassifiers",
 ]
 install_requires: List[str] = [
     "pytconf",
     "pylogconf",
 ]
 make_requires: List[str] = [
-    "pyclassifiers",
     "pydmt",
     "pymakehelper",
 ]
